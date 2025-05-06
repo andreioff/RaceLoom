97bd70f8d1dcb64114b0c42bd8df3eb2228e6fb5
Andrei Covaci <andreioff@yahoo.ro>
2025-05-06 15:53:30 +0200

    Move execution time dictionary to abstract class

--- src/decorators/exec_time.py
+++ src/decorators/exec_time.py
@@ -1,18 +1,27 @@
 from __future__ import annotations
+
+from abc import ABC
 from functools import wraps
+from math import fsum
 from time import perf_counter
 from typing import Protocol, Dict, Callable, Concatenate
-from math import fsum
 
 
-class PExecTimes(Protocol):
+class _PExecTimes(Protocol):
     execTimes: Dict[str, float]
+
+
+class ExecTimes(_PExecTimes, ABC):
+    """Class for storing execution times of methods."""
+
+    def __init__(self) -> None:
+        self.execTimes: Dict[str, float] = {}
 
     def getTotalExecTime(self) -> float:
         return fsum(self.execTimes.values())
 
 
-def with_time_execution[M: PExecTimes, **P, R](
+def with_time_execution[M: _PExecTimes, **P, R](
     method: Callable[Concatenate[M, P], R],
 ) -> Callable[Concatenate[M, P], R]:
     """Decorator to time method execution and store it in an instance attribute."""

f03ec4f5dc579a0d7bcf424886e4264ea07e86d6
Andrei Covaci <andreioff@yahoo.ro>
2025-05-31 17:05:52 +0200

    Build network policy for every rcfg after trace generation

--- src/tracer.py
+++ src/tracer.py
@@ -6,14 +6,60 @@ from src.generator.trace_generator_factory import (TraceGenOption,
                                                    newTraceGenerator)
 from src.generator.trace_tree import TraceTree
 from src.KATch_comm import KATchComm
-from src.model.dnk_maude_model import DNKMaudeModel
+from src.model.dnk_maude_model import DNKMaudeModel, ElementType
 from src.stats import StatsEntry
+from src.trace.transition import RcfgTrans
 from src.tracer_config import TracerConfig
+from src.util import DyNetKATSymbols as sym
 from src.util import createDir, exportFile
 
 _TRACES_FILE_NAME = "traces"
 _HARMFUL_TRACES_DIR_NAME = "harmful_traces"
 _HARMFUL_TRACES_RAW_DIR_NAME = "harmful_traces_raw"
+
+
+class TracerError(Exception):
+    pass
+
+
+def _buildNetworkPolicy(fts: List[str], link: str) -> str:
+    if not fts:
+        return sym.ZERO
+    ftsStr = f" {sym.OR} ".join(fts)
+    oneStepStr = f"({ftsStr}) {sym.AND} ({link})"
+    return f"({oneStepStr}) {sym.AND} ({oneStepStr}){sym.STAR}"
+
+
+def _reconstructRcfgs(traceTree: TraceTree) -> None:
+    """Iterates through all traces in the tree and applies the update of any
+    switch reconfiguration to the flow table of the target switch up until that
+    point, then assigns the policy of the reconfiguration to the result"""
+    dm = traceTree.dnkModel
+    nodeIdToOldPolicy: dict[int, str] = {}
+    for trace in traceTree.getTraceIterator():
+        # tracks the flow tables of all elements throughout the trace
+        currNet = [metad.initialFTs.copy() for metad in dm.elsMetadata]
+        for node in trace:
+            # anything that is not a reconfiguration to a switch is skipped
+            if not isinstance(node.trans, RcfgTrans):
+                continue
+            metad = dm.elsMetadata[node.trans.dstPos]
+            if metad.pType != ElementType.SW:
+                continue
+            fts = currNet[node.trans.dstPos]
+            swToModify = metad.findSwitchIndex(node.trans.channel)
+            if swToModify == -1:
+                raise TracerError(
+                    "Could not match network switch based on rcfg channel"
+                )
+            # apply the reconfiguration to the target element and store the result
+            oldPolicy = nodeIdToOldPolicy.get(node.id, None)
+            if oldPolicy is not None:
+                fts[swToModify] = oldPolicy
+                continue
+            fts[swToModify] = node.trans.policy
+            nodeIdToOldPolicy[node.id] = node.trans.policy
+            node.trans.setPolicy(_buildNetworkPolicy(fts, metad.link))
 
 
 class Tracer:
@@ -22,51 +68,52 @@ class Tracer:
     ) -> None:
         self.config = config
         self.dnkModel = dnkModel
-        self.__traceGen = newTraceGenerator(genStrategy, config)
-        self.__traceTree: TraceTree = TraceTree(self.dnkModel)
-        self.__initTraceAnalyzer()
+        self._traceGen = newTraceGenerator(genStrategy, config)
+        self._traceTree: TraceTree = TraceTree(self.dnkModel)
+        self._initTraceAnalyzer()
 
-    def __initTraceAnalyzer(self) -> None:
+    def _initTraceAnalyzer(self) -> None:
         outputDirRaw = os.path.join(
             self.config.outputDirPath, _HARMFUL_TRACES_RAW_DIR_NAME
         )
         outputDirDOT = os.path.join(self.config.outputDirPath, _HARMFUL_TRACES_DIR_NAME)
         createDir(outputDirRaw)
         createDir(outputDirDOT)
-        self.__katchComm = KATchComm(self.config.katchPath, self.config.outputDirPath)
-        self.__traceAnalyzer = TracesAnalyzer(
-            self.__katchComm, outputDirRaw, outputDirDOT
+        self._katchComm = KATchComm(self.config.katchPath, self.config.outputDirPath)
+        self._traceAnalyzer = TracesAnalyzer(
+            self._katchComm, outputDirRaw, outputDirDOT
         )
 
     def generateTraces(self, depth: int) -> bool:
-        self.__traceTree = self.__traceGen.run(self.dnkModel, depth)
+        self._traceTree = self._traceGen.run(self.dnkModel, depth)
+        _reconstructRcfgs(self._traceTree)
 
-        if self.__traceTree.traceCount() == 0:
+        if self._traceTree.traceCount() == 0:
             return False
-        self.__writeTracesToFile()
+        self._writeTracesToFile()
         return True
 
-    def __writeTracesToFile(self) -> None:
+    def _writeTracesToFile(self) -> None:
         tracesFilePath = os.path.join(
             self.config.outputDirPath,
             f"{_TRACES_FILE_NAME}_{self.config.inputFileName}.txt",
         )
         exportFile(
             tracesFilePath,
-            os.linesep.join([str(t) for t in self.__traceTree.getTraceIterator()]),
+            os.linesep.join([str(t) for t in self._traceTree.getTraceIterator()]),
         )
 
     def analyzeTraces(self) -> None:
-        self.__traceAnalyzer.run(self.__traceTree, self.dnkModel.getElementsMetadata())
+        self._traceAnalyzer.run(self._traceTree, self.dnkModel.getElementsMetadata())
 
     def getTraceGenerationStats(self) -> List[StatsEntry]:
-        return self.__traceGen.getStats()
+        return self._traceGen.getStats()
 
     def getTraceAnalysisStats(self) -> List[StatsEntry]:
-        return self.__katchComm.getStats() + self.__traceAnalyzer.getStats()
+        return self._katchComm.getStats() + self._traceAnalyzer.getStats()
 
     def getTotalExecTime(self) -> float:
         return (
-            self.__traceGen.getWrapperTotalExecTime()
-            + self.__traceAnalyzer.getWrapperTotalExecTime()
+            self._traceGen.getWrapperTotalExecTime()
+            + self._traceAnalyzer.getWrapperTotalExecTime()
         )

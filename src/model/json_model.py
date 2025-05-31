0733ccc02f72c3a4e07da27c10374e73198da602
Andrei Covaci <andreioff@yahoo.ro>
2025-05-31 10:24:15 +0200

    Force channel names to be unique for every switch in a network

--- src/model/json_model.py
+++ src/model/json_model.py
@@ -1,7 +1,8 @@
 from typing import List, Optional
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, ValidationError, field_validator
 from pydantic.types import StringConstraints
+from pydantic_core import PydanticCustomError
 from typing_extensions import Annotated
 
 NonEmptyString = Annotated[str, StringConstraints(min_length=1)]
@@ -43,3 +44,33 @@ class DNKNetwork(BaseModel):  # type: ignore
                 "DyNetKAT Network must contain at least 1 switch and 1 controller"
             )
         return v
+
+
+def validateSwitchChannels(model: DNKNetwork) -> None:
+    """Raises ValidationError if the switches across all networks in the given
+    JSON model do not use unique channels"""
+
+    def raiseError(channel: str, swName: str) -> None:
+        raise PydanticCustomError(
+            "Re-used switch channel",
+            f"Channel '{channel}' cannot be re-used for switch '{swName}'. "
+            + "A channel name used to receive updates for "
+            + "a switch in a network cannot be re-used for any other switch.",
+        )
+
+    channels: dict[str, bool] = {}
+
+    for name, sw in model.Switches.items():
+        swChannels: dict[str, bool] = {}
+        for du in sw.DirectUpdates:
+            if du.Channel in channels:
+                raiseError(du.Channel, name)
+            swChannels[du.Channel] = True
+        for ru in sw.RequestedUpdates:
+            if ru.RequestChannel in channels:
+                raiseError(ru.RequestChannel, name)
+            if ru.ResponseChannel in channels:
+                raiseError(ru.ResponseChannel, name)
+            swChannels[ru.RequestChannel] = True
+            swChannels[ru.ResponseChannel] = True
+        channels.update(swChannels)
